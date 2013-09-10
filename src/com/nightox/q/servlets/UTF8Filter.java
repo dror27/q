f77d6a5c520d30ec48ba6394445f00dd4363c8f2
Dror Kessler <dror27.kessler@gmail.com>
2013-09-10 16:23:03 +0300

    fix soundcloud link + no cache

--- src/com/nightox/q/servlets/UTF8Filter.java
+++ src/com/nightox/q/servlets/UTF8Filter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet Filter implementation class UTF8Filter
@@ -33,9 +34,16 @@ public class UTF8Filter implements Filter {
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException 
 	{
+		// UTF-8
 		request.setCharacterEncoding("UTF-8");
 		response.setCharacterEncoding("UTF-8");
 
+		// no cache
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		httpResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+		httpResponse.setHeader("Pragma", "no-cache"); // HTTP 1.0
+		httpResponse.setDateHeader("Expires", 0); // Proxies.
+		
 		// pass the request along the filter chain
 		chain.doFilter(request, response);
 	}
